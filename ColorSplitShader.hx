0c9f475b5699b4e10c585d9812721796741f753d
Isaac Denner <iadenner@gmail.com>
2021-07-19 18:21:45 -0400

    Closer to a traditional color split shader

--- ColorSplitShader.hx
+++ ColorSplitShader.hx
@@ -32,19 +32,19 @@ void main() {
 		float b = 0.0;
 		
 		//checks if the image is non-transparent at textureCoordv.x - borderwidth. If it is solid, add r to the image
-		if (flixel_texture2D(bitmap, vec2((openfl_TextureCoordv.x - borderWidth), openfl_TextureCoordv.y)).a != 0.)
+		if (flixel_texture2D(bitmap, vec2((openfl_TextureCoordv.x - borderWidth), openfl_TextureCoordv.y)).a != 0.)// || flixel_texture2D(bitmap, vec2((openfl_TextureCoordv.x + borderWidth), openfl_TextureCoordv.y)).a != 0.)
 		{
 			r = 0.3;
 		}
 		
 		//do the same for blue, but at a different offset
-		if (flixel_texture2D(bitmap, vec2((openfl_TextureCoordv.x - borderWidth*0.5), (openfl_TextureCoordv.y - borderWidth*0.5))).a != 0.)
+		if (flixel_texture2D(bitmap, vec2((openfl_TextureCoordv.x - borderWidth*0.5), (openfl_TextureCoordv.y + borderWidth*0.5))).a != 0.)// || flixel_texture2D(bitmap, vec2((openfl_TextureCoordv.x - borderWidth*0.5), (openfl_TextureCoordv.y + borderWidth*0.5))).a != 0.)
 		{
 			g = 0.3;
 		}
 		
 		//and finally for green, but this time at texturecoordv.y - borderwidth. You can switch up the -s here for +s or whatever to get different combinations of r / g / b
-		if (flixel_texture2D(bitmap, vec2((openfl_TextureCoordv.x), (openfl_TextureCoordv.y - borderWidth*0.5))).a != 0.)
+		if (flixel_texture2D(bitmap, vec2((openfl_TextureCoordv.x), (openfl_TextureCoordv.y - borderWidth))).a != 0.)// || flixel_texture2D(bitmap, vec2((openfl_TextureCoordv.x), (openfl_TextureCoordv.y + borderWidth))).a != 0.)
 		{
 			b = 0.3;
 		}
@@ -59,7 +59,7 @@ void main() {
 }
 	')
 	
-	var borderSize:Float = 0.05;
+	var borderSize:Float = 0.04;
 	public function new() 
 	{
 		super();
